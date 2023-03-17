675029a8f2022b2062dec4f2287ee10eb9a80cb7
Agnieszka Nowacka <aguo84@gmail.com>
2023-03-17 13:13:51 +0100

    Refactor code in login test methods

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,6 +1,5 @@
 import PageObjects.LoginPage;
 import Utils.DataHandler;
-import dev.failsafe.internal.util.Assert;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
@@ -9,15 +8,19 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.PageFactory;
 
 import java.time.Duration;
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class LoginTests {
     private WebDriver driver;
     private LoginPage loginPage;
-    private String folderPath = "src\\test\\java\\TestData\\";
     private XSSFSheet excelSheet;
+    private String folderPath = "src\\test\\java\\TestData\\";
     private String excelFileName = "UsersData.xlsx";
-    private String sheetName = "UsernameCredentials";
+    private List<String> sheetNamesList = new ArrayList<>(Arrays.asList("UsernameCredentials", "UserEmailCredentials"));
+    private String userName;
+    private String userPass;
 
     @BeforeEach
     public void setup() {
@@ -33,41 +36,32 @@ public class LoginTests {
 
     @Test
     public void loginWithUsername_multipleUnregisteredUsers_shouldFail() {
+        excelSheet = DataHandler.readExcel(folderPath + excelFileName, sheetNamesList.get(0));
+        int rowsCounter = excelSheet.getPhysicalNumberOfRows();
 
-        excelSheet = DataHandler.readExcel(folderPath + excelFileName, sheetName);
-        int rowsNum = excelSheet.getPhysicalNumberOfRows();
-
-        for (int i = 0; i < rowsNum - 1; i++) {
-            String userName = excelSheet.getRow(i + 1).getCell(0).getStringCellValue();
-            String userPass = excelSheet.getRow(i + 1).getCell(1).getStringCellValue();
-            loginPage.sendUsername(userName).sendPassword(userPass).login();
+        for (int i = 0; i < rowsCounter - 1; i++) {
+            sendLoginCredentialsFromExcelFile(i + 1, 0);
 
             Assertions.assertEquals(loginPage.getErrorAlertMessage(), "Błąd: Brak "
                             + userName
                             + " "
                             + "wśród zarejestrowanych w witrynie użytkowników. Jeśli nie masz pewności co do nazwy użytkownika, użyj adresu e-mail.",
                     "Error message is incorrect");
-
             loginPage.clearLoginForm();
         }
     }
 
     @Test
     public void loginWithUserEmail_multipleUnregisteredUsers_shouldFail() {
-
-        sheetName = "UserEmailCredentials";
-        excelSheet = DataHandler.readExcel(folderPath + excelFileName, sheetName);
+        excelSheet = DataHandler.readExcel(folderPath + excelFileName, sheetNamesList.get(1));
         int rowsNum = excelSheet.getPhysicalNumberOfRows();
 
         for (int i = 0; i < rowsNum - 1; i++) {
-            String userEmail = excelSheet.getRow(i + 1).getCell(0).getStringCellValue();
-            String userPass = excelSheet.getRow(i + 1).getCell(1).getStringCellValue();
-            loginPage.sendUsername(userEmail).sendPassword(userPass).login();
+            sendLoginCredentialsFromExcelFile(i + 1, 0);
 
             Assertions.assertEquals(loginPage.getErrorAlertMessage(),
                     "Nieznany adres e-mail. Proszę sprawdzić ponownie lub wypróbować swoją nazwę użytkownika.",
                     "Error message is incorrect");
-
             loginPage.clearLoginForm();
         }
     }
@@ -75,30 +69,30 @@ public class LoginTests {
     @Test
     public void login_randomUser_NoUserNameGiven_shouldFail() {
 
-        excelSheet = DataHandler.readExcel(folderPath + excelFileName, sheetName);
+        excelSheet = DataHandler.readExcel(folderPath + excelFileName, sheetNamesList.get(0));
         int rowsNum = excelSheet.getPhysicalNumberOfRows();
-        //System.out.println(rowsNum);
-
-        String userPass = excelSheet.getRow(DataHandler.getRandomNumber(1, rowsNum)).getCell(1).getStringCellValue();
+        userPass = excelSheet.getRow(DataHandler.getRandomNumber(1, rowsNum)).getCell(1).getStringCellValue();
         loginPage.sendPassword(userPass).login();
 
         Assertions.assertEquals(loginPage.getErrorAlertMessage(), "Błąd: Nazwa użytkownika jest wymagana.");
-
         loginPage.clearLoginForm();
     }
 
     @Test
     public void login_randomUser_NoPasswordGiven_shouldFail() {
-
-        excelSheet = DataHandler.readExcel(folderPath + excelFileName, sheetName);
+        excelSheet = DataHandler.readExcel(folderPath + excelFileName, sheetNamesList.get(0));
         int rowsNum = excelSheet.getPhysicalNumberOfRows();
-
-        String userName = excelSheet.getRow(DataHandler.getRandomNumber(1, rowsNum)).getCell(0).getStringCellValue();
+        userName = excelSheet.getRow(DataHandler.getRandomNumber(1, rowsNum)).getCell(0).getStringCellValue();
         loginPage.sendUsername(userName).login();
 
         Assertions.assertEquals(loginPage.getErrorAlertMessage(), "Błąd: Hasło jest puste.");
-
         loginPage.clearLoginForm();
+    }
+
+    private void sendLoginCredentialsFromExcelFile(int rowNum, int cellNum) {
+        userName = excelSheet.getRow(rowNum).getCell(cellNum).getStringCellValue();
+        userPass = excelSheet.getRow(rowNum).getCell(cellNum + 1).getStringCellValue();
+        loginPage.sendUsername(userName).sendPassword(userPass).login();
     }
 
     @AfterEach
